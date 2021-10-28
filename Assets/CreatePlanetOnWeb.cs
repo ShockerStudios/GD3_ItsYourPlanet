3deac900d02dedec9f2518e3dfcc051ce573f52d
Kelly Ray J <47062909+kellyrayj@users.noreply.github.com>
2021-10-28 13:25:13 -0500

    Stuff

--- Assets/CreatePlanetOnWeb.cs
+++ Assets/CreatePlanetOnWeb.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using System;
 
 public class CreatePlanetOnWeb : MonoBehaviour
 {
+    public Planet myPlanet;
+
     public void Start()
     {
-        GetComponent<Planet>().GeneratePlanet();
+        myPlanet = GetComponent<Planet>();
+        myPlanet.GeneratePlanet();
         transform.position = new Vector3(0, -500, -500);
+    }
+
+    public void DoSetShape(ShapeSettings shapes)
+    {
+        myPlanet.shapeSettings = shapes;
+    }
+
+    public void DoSetColors(ColourSettings colors)
+    {
+        myPlanet.colourSettings = colors;
+    }
+
+    public void RandomizeSettings()
+    {
+        myPlanet.shapeSettings.noiseLayers[0].noiseSettings.simpleNoiseSettings.strength = UnityEngine.Random.Range(0.05f, .8f);
+        myPlanet.shapeSettings.noiseLayers[0].noiseSettings.simpleNoiseSettings.centre = UnityEngine.Random.insideUnitSphere * UnityEngine.Random.Range(1f, 100f);
+        myPlanet.GeneratePlanet();
     }
 }
